107104b5573a580253e584a59602d1b1ecdbf637
Danny Hermes <daniel.j.hermes@gmail.com>
2015-03-08 17:22:54 -0700

    Re-writing foo_mex.c to use both dimensions of input matrix.

--- foo_mex.c
+++ foo_mex.c
@@ -4,12 +4,12 @@
 #include "foo.h"
 #include "mex.h"
 
-void call_py(double *zap, size_t zap_size, size_t zap_rows)
+void call_py(double *zap, size_t zap_size, size_t zap_rows, size_t zap_cols)
 {
     void* handle = dlopen("libpython2.7.so", RTLD_LAZY | RTLD_GLOBAL);
     Py_Initialize();
     initfoo();
-    from_waldo(zap, zap_rows);
+    from_waldo(zap, zap_size, zap_rows, zap_cols);
     Py_Finalize();
     dlclose(handle);
 }
@@ -20,24 +20,21 @@ void mexFunction( int nlhs, mxArray *plhs[],
     double *out_matrix;
     size_t zap_size;
     size_t zap_rows;
+    size_t zap_cols;
 
-    if (nrhs != 2) {
-        mexErrMsgIdAndTxt("foo_mex:nrhs", "Two inputs required.");
+    if (nrhs != 1) {
+        mexErrMsgIdAndTxt("foo_mex:nrhs", "One input required.");
     }
     if (nlhs != 1) {
         mexErrMsgIdAndTxt("foo_mex:nlhs", "One output required.");
     }
 
-    if (mxGetM(prhs[0]) != 1) {
-        mexErrMsgIdAndTxt("foo_mex:row_vec",
-                          "First input expected to be row vector.");
-    }
-    zap_size = mxGetN(prhs[0]);
-    /* TODO: Is there a better way to get a size_t scalar? */
-    zap_rows = mxGetScalar(prhs[1]);
+    zap_rows = mxGetM(prhs[0]);
+    zap_cols = mxGetN(prhs[0]);
+    zap_size = zap_rows * zap_cols;
 
     /* Create a copy of input to be modified. */
     plhs[0] = mxDuplicateArray(prhs[0]);
     out_matrix = mxGetPr(plhs[0]);
-    call_py(out_matrix, zap_size, zap_rows);
+    call_py(out_matrix, zap_size, zap_rows, zap_cols);
 }
