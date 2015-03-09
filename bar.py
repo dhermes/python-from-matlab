4d6bc6e0f61916cd345d092dee720ce8050473c1
Danny Hermes <daniel.j.hermes@gmail.com>
2015-03-08 17:03:44 -0700

    Surfacing matrix information in bar.py.
    This is to highlight the fact that C ordering is used instead
    of Fortran ordering (which is the MATLAB default).

--- bar.py
+++ bar.py
@@ -1,13 +1,15 @@
+DEBUG = True
+
+
 def waldo(J):
-    # # DEBUG MODE:
-    # if True:
-    #     print '=' * 60
-    #     print 'type(J): %r' % (type(J),)
-    #     print 'J.dtype: %r' % (J.dtype,)
-    #     print 'J.shape: %r' % (J.shape,)
-    #     print 'J:'
-    #     print J
-    #     print 'J.flags:'
-    #     print J.flags
-    #     print '=' * 60
+    if DEBUG:
+        print '=' * 60
+        print 'type(J): %r' % (type(J),)
+        print 'J.dtype: %r' % (J.dtype,)
+        print 'J.shape: %r' % (J.shape,)
+        print 'J:'
+        print J
+        print 'J.flags:'
+        print J.flags
+        print '=' * 60
     J *= 2.1
